0531155883a20b2c6d49bc5bec98c658a7489e27
name <nicolaspeluso125@gmai.com>
2023-09-20 07:45:59 -0300

    aprendemos sobre Templates, urls, Segurança

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse;
 
 def index(req):
-    return HttpResponse('<h1>Nicolas peluso</h1>');
+    return render(req, 'index.html');
